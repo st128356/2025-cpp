924bc4454af03ef84104ae37e91ce347f32b8e07
Ksenia Mamonova <ksymmamonovka@gmail.com>
2025-09-17 19:07:45 +0300

    lesson 1 code upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome to Online IDE!! Happy Coding :)";
+    return 0;
+}
